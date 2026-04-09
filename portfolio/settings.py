ac29c04bee2201d3dcf403900dbc54b13b618dc3
pachuau10 <duhteapachuau10@gmail.com>
2026-04-09 12:54:44 +0530

    error 404 again

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -72,7 +72,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',    # your static folder for development
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # where collectstatic will output production files
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
